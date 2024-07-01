3333adaf8c3dfbe19caa90490e469a15a3e7e7b1
Pranjal Verma <pvcodes@gmail.com>
2024-07-01 15:53:10 +0530

    init vuejs integration

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+window.addEventListener('message', (event) => {
+  if (event.origin === "https://flow.viasocket.com") {
+  const receivedData = event.data;
+  }
+  });
+</script>
+
+<template>
+  <div id="main">
+    <p>HELLO from viasocket</p>
+   <button id="viasocket-embed-open-button" onclick="openViasocket(flowId, serviceData)" >Open Integrations</button>
+
+  </div>
+    <!-- <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a> -->
+</template>
+
+
+<style scoped>
+#main {
+  /* color: #888; */
+  height: 100%;
+}
+</style>
---
+++ src/components/Integration.vue
@@ -0,0 +1,11 @@
+<script setup>
+window.addEventListener('message', (event) => {
+  if (event.origin === "https://flow.viasocket.com") {
+  const receivedData = event.data;
+  }
+  });
+</script>
+
+<template>
+   <button id="viasocket-embed-open-button" onclick="openViasocket(flowId, serviceData)" >Open Integrations</button>
+</template>
