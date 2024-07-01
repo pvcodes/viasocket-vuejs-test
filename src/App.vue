ea33613cb62dc92d9189fb189d3f7ba666229e55
Pranjal Verma <pvcodes@gmail.com>
2024-07-01 15:59:56 +0530

    refactore

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script setup>
-window.addEventListener('message', (event) => {
-  if (event.origin === "https://flow.viasocket.com") {
-  const receivedData = event.data;
-  }
-  });
+import Integration from "./components/Integration.vue";
 </script>
 
 <template>
-  <div id="main">
-    <p>HELLO from viasocket</p>
-   <button id="viasocket-embed-open-button" onclick="openViasocket(flowId, serviceData)" >Open Integrations</button>
-
-  </div>
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
+	<div id="main">
+    <h2>Hello from viasocket</h2>  
+		<Integration />
+	</div>
 </template>
-
 
 <style scoped>
 #main {
-  /* color: #888; */
-  height: 100%;
+	height: 100%;
 }
 </style>
--- src/components/Integration.vue
+++ src/components/Integration.vue
@@ -1,11 +1,24 @@
-<script setup>
-window.addEventListener('message', (event) => {
-  if (event.origin === "https://flow.viasocket.com") {
-  const receivedData = event.data;
-  }
-  });
+<script>
+window.addEventListener("message", (event) => {
+	if (event.origin === "https://flow.viasocket.com") {
+		const receivedData = event.data;
+	}
+});
 </script>
 
 <template>
-   <button id="viasocket-embed-open-button" onclick="openViasocket(flowId, serviceData)" >Open Integrations</button>
+	<div id="integration">
+		<button
+			id="viasocket-embed-open-button"
+			onclick="openViasocket(flowId, serviceData)"
+		>
+			Open Integrations
+		</button>
+	</div>
 </template>
+
+<style scoped>
+#integration {
+	height: 100%;
+}
+</style>
